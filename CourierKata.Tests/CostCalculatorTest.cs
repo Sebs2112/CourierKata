27ab731968e47ecc3dbe353e0d22cda0bb58883d
sebs2112 <sebs2112@outlook.com>
2021-11-02 15:00:31 +0000

    Created test for one of each parcel size to be added to invoice.

--- CourierKata.Tests/CostCalculatorTest.cs
+++ CourierKata.Tests/CostCalculatorTest.cs
@@ -59,5 +59,27 @@ namespace CourierKata.Tests
             Assert.Equal(EXPECTED_INVOICE_SUMMARY, invoice.ToString());
         }
 
+        [Fact]
+        public void CalculateInvoice_OneOfEachParcelSize_CombinedListAndTotalCost()
+        {
+            //Arrange
+            const string EXPECTED_INVOICE_SUMMARY = "Small Parcel: $3\n" +
+                                                    "Medium Parcel: $8\n" +
+                                                    "Large Parcel: $15\n" +
+                                                    "XL Parcel: $25\n" +
+                                                    "Total Cost: $51";
+            var order = new Order();
+            order.AddParcelAttributes(new ParcelAttributes(1, 1, 1));
+            order.AddParcelAttributes(new ParcelAttributes(10, 10, 10));
+            order.AddParcelAttributes(new ParcelAttributes(50, 50, 50));
+            order.AddParcelAttributes(new ParcelAttributes(100, 100, 100));
+            var costCalculator = new CostCalculator();
+            //Act
+            var invoice = costCalculator.CalculateInvoice(order);
+            //Assert
+
+            Assert.Equal(EXPECTED_INVOICE_SUMMARY, invoice.ToString());
+        }
+
     }
 }
