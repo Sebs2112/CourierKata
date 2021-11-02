24243f7fa45eb34ee31ce79e20c0405b77427823
sebs2112 <sebs2112@outlook.com>
2021-11-02 13:53:24 +0000

    Added First test for small package

--- CourierKata.Tests/UnitTest1.cs
+++ CourierKata.Tests/UnitTest1.cs
@@ -3,5 +3,19 @@ using Xunit;
 
 namespace CourierKata.Tests
 {
-
+    public class CostCalculatorTest
+    {
+        [Fact]
+        public void CalculateInvoice_MaxDimensionLessThan10_SmallPackageOnSummary()
+        {
+            //Arrange
+            const string EXPECTED_INVOICE_SUMMARY = "Small Parcel:\n$3 Total Cost: $3";
+            var parcelAttributes = new ParcelAttributes(1, 1, 1);
+            var costCalulator = new CostCalculator();
+            //Act
+            var invoice = costCalulator.CalculateInvoice(parcelAttributes);
+            //Assert
+            Assert.Equal(EXPECTED_INVOICE_SUMMARY, invoice.ToString());
+        }
+    }
 }
